f42b1b68a01af08b6bd65ec8ef3b0f0edbe4f9b8
Jomaica Lei <jalfiler@seattleu.edu>
2024-01-25 17:39:13 -0800

    commit files

---
+++ lab_3/lab3_final_code/run_experiments.py
@@ -0,0 +1,34 @@
+from batch_simulator import run_agents_in_environment
+
+#######################################
+# Two options about what to do with the agent's log messages in absence 
+# of a GUI pane
+def log_to_console(msg):
+    print(msg)
+
+def log_null(msg):
+    pass
+
+########################################
+    
+dirt_density = 0.1
+wall_density = 0.3
+num_samples = 100
+output_file_name = 'simulation_results.csv'
+agents = [ ("agents/reactiveagent.py",  "NoSenseAgent"),
+           ("agents/reactiveagent.py",   "SenseDirtAgent"),
+           ("agents/agentworldmodel.py", "AgentWorldModel"),
+           ("agents/macroactionagent.py", "MacroActionAgent"),
+           ("agents/planningagent.py", "PlanningAgent"),
+           ("agents/gohomeagent.py", "GoHomeAgent")
+         ]
+write_results_to_console = True
+
+
+run_agents_in_environment(dirt_density, 
+                          wall_density, 
+                          agents, 
+                          log_null, 
+                          num_samples, 
+                          output_file_name, 
+                          write_results_to_console)
